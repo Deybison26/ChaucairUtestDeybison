944829cfced4b1591b686ff8d3d6663205c8bd5c
Deybison26 <deybison2026@gmail.com>
2022-03-31 18:31:43 -0500

    automatizacion utest

---
+++ build/resources/test/features/utest.feature
@@ -0,0 +1,18 @@
+# Autor Deybison
+# language:en
+
+@stories
+Feature: Utest
+  As a user, I want to learn how to automate in screamplay at Utest page practice
+  @scenario1
+  Scenario: Login the user
+    Given than deybison wants to learn automation at the utest
+      | strUsername               | strPassword  |
+      | deybison2026@gmail.com    | Enano12345*  |
+    When he finds the coursed called utest
+    Then the user Logout with their credentials
+  @scenario2
+  Scenario: Register a new user
+    Given set data in fields
+      | strFirstName  | strLastName | strEmailAddress                 | strCity    | strZipCode | strPassword    |
+      | julio         | garzon      | fosewaulaugro-9510@yopmail.com | Popayán    | 190002     | Andres123456*   |
---
+++ src/main/java/co.com.choucair.certification.utest/userinterface/UtestRegisterPage.java
@@ -0,0 +1,72 @@
+package co.com.choucair.certification.utest.userinterface;
+
+import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.screenplay.targets.Target;
+import org.openqa.selenium.By;
+
+public class UtestRegisterPage extends PageObject {
+    public static final Target REGISTER_BUTTON = Target.the("button register")
+            .located(By.xpath("//a[contains(text(), 'Join Today')]"));
+
+    public static final Target INPUT_FIRST_NAME = Target.the("where do we write the first name")
+            .located(By.id("firstName"));//id
+
+    public static final Target INPUT_LAST_NAME = Target.the("where do we write the last name")
+            .located(By.id("lastName"));//id
+
+    public static final Target INPUT_EMAIL_ADDRESS = Target.the("where do we write the email")
+            .located(By.id("email"));//id
+
+    public static final Target SELECT_DATE_MONTH = Target.the("where do we write the month")
+            .located(By.id("birthMonth"));//id
+
+    public static final Target OPTION_DATE_MONTH = Target.the("where do we write the month")
+            .located(By.xpath("//option[contains(text(), 'January')]"));//id
+
+    public static final Target SELECT_DATE_DAY = Target.the("where do we write the day")
+            .located(By.id("birthDay"));//id
+
+    public static final Target OPTION_DATE_DAY = Target.the("where do we write the day")
+            .located(By.xpath("//option[contains(text(), '1')]"));//id
+
+    public static final Target SELECT_DATE_YEAR = Target.the("where do we write the year")
+            .located(By.id("birthYear"));//id
+
+    public static final Target OPTION_DATE_YEAR = Target.the("where do we write the year")
+            .located(By.xpath("//option[contains(text(), '1990')]"));//id
+
+    public static final Target NEXT_LOCATION = Target.the("next button")
+            .located(By.xpath("//span[contains(text(), 'Next: Location')]"));//id
+
+    public static final Target INPUT_CITY = Target.the("where do we write the city")
+            .located(By.id("city"));//id
+
+    public static final Target INPUT_ZIP_CODE = Target.the("where do we write the zip code")
+            .located(By.id("zip"));//id
+
+    public static final Target SELECT_COUNTRY = Target.the("where do we write the country")
+            .located(By.xpath("//span[contains(text(), 'Colombia')]"));//id
+
+    public static final Target NEXT_DEVICES = Target.the("next button")
+            .located(By.xpath("//span[contains(text(), 'Next: Devices')]"));//id
+
+    //
+    public static final Target NEXT_LAST_STEP = Target.the("Final button")
+            .located(By.xpath("//span[contains(text(), 'Next: Last Step')]"));//id
+    //
+
+    public static final Target INPUT_PASSWORD1 = Target.the("where do we write the password")
+            .located(By.id("password"));//id
+
+    public static final Target INPUT_PASSWORD2 = Target.the("where do we write the confirmPassword")
+            .located(By.id("confirmPassword"));//id
+
+    public static final Target SELECT_TERMS = Target.the("where do we select the terms")
+            .located(By.id("termOfUse"));//id
+
+    public static final Target SELECT_PRIVACY = Target.the("where do we select the privacy")
+            .located(By.id("privacySetting"));//id
+
+    public static final Target BUTTON_REGISTER = Target.the("button register")
+            .located(By.xpath("//span[contains(text(), 'Complete Setup')]"));//id
+}
